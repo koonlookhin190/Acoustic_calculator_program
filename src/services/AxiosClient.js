3762a30ff7bfea24cd2860478d9c65204a4f7bd8
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-04-27 20:42:43 +0700

    finish calculate

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080',
-
+  baseURL: 'http://127.0.0.1:5000',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
