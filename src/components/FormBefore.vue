4de0eee2c398f933067553f8d11da398e3b7ea91
Yanwarut Suksawat <yanwarut_s@cmu.ac.th>
2023-04-29 15:45:28 +0700

    Add FormBefore

---
+++ src/components/FormBefore.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="row">
+    <div>
+      <div class="container2 greybox">
+        <div class="row">
+          <div class="inputcol col-lg-4">
+            <span> ผนังด้านหน้า </span>
+            <input class="inputbox" type="text" />
+            <BaseSelectVue
+              :options="GStore.material"
+              v-model="materialSet.material1"
+            />
+            <BaseSelectVue
+              :options="GStore.material"
+              v-model="materialSet.material2"
+            />
+          </div>
+          <div class="dropdownlist col-lg-4"></div>
+          <div class="col-lg-4">
+            <span class="addBox">+ Add materials</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{ materialSet.material1 }}
+    {{ materialSet.material2 }}
+    {{ GStore.keep }}
+    <button @click="calCulate">Presss it</button>
+  </div>
+</template>
+
+<script>
+import BaseSelectVue from '@/components/BaseSelect.vue'
+export default {
+  inject: ['GStore'],
+  components: {
+    BaseSelectVue
+  },
+  data() {
+    return {
+      materialSet: {
+        material1: {},
+        material2: {},
+        material3: {},
+        material4: {},
+        material5: {}
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.greybox {
+  margin: auto;
+  width: 50%;
+  border: solid;
+  padding: 10px;
+  background-color: #d9d9d9;
+}
+.inputcol {
+  display: inline-block;
+}
+.inputbox {
+  margin-top: 5px;
+  margin-right: 20px;
+  width: 50px;
+}
+.dropdownlist {
+  margin-top: 5px;
+  width: 100px;
+}
+.addBox {
+  margin-left: 60px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -145,34 +145,7 @@
           ให้ใช้วิธีเดียวกันในนี้ในด้านที่มีประตูและหน้าต่าง</span
         >
       </div>
-    </div>
-    <div class="row">
-      <div>
-        <div class="container2 greybox">
-          <div class="row">
-            <div class="inputcol col-lg-4">
-              <span> ผนังด้านหน้า </span>
-              <input class="inputbox" type="text" />
-              <BaseSelectVue
-                :options="GStore.material"
-                v-model="materialSet.material1"
-              />
-              <BaseSelectVue
-                :options="GStore.material"
-                v-model="materialSet.material2"
-              />
-            </div>
-            <div class="dropdownlist col-lg-4"></div>
-            <div class="col-lg-4">
-              <span class="addBox">+ Add materials</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      {{ materialSet.material1}}
-      {{ materialSet.material2}}
-      {{ GStore.keep }}
-      <button @click="calCulate">Presss it</button>
+      <FormBefore />
     </div>
   </div>
 </template>
@@ -180,14 +153,14 @@
 <script>
 import CalculateService from '@/services/CalculateService.js'
 import { Form, Field } from 'vee-validate'
+import FormBefore from '@/components/FormBefore.vue'
 import * as yup from 'yup'
-import BaseSelectVue from '@/components/BaseSelect.vue'
 export default {
   inject: ['GStore'],
   components: {
     Form,
     Field,
-    BaseSelectVue
+    FormBefore
   },
   data() {
     const schema = yup.object().shape({
@@ -196,14 +169,7 @@ export default {
       height: yup.string()
     })
     return {
-      schema,
-      materialSet: {
-        material1: {},
-        material2: {},
-        material3: {},
-        material4: {},
-        material5: {}
-      }
+      schema
     }
   },
   methods: {
@@ -211,8 +177,7 @@ export default {
       console.log(schema)
       console.log(JSON.stringify(this.material))
       CalculateService.getCalculate(schema)
-    },
-    calCulate() {}
+    }
   }
 }
 </script>
@@ -272,27 +237,5 @@ span {
 .button {
   margin-top: 20px;
   margin-right: 40px;
-}
-.greybox {
-  margin: auto;
-  width: 50%;
-  border: solid;
-  padding: 10px;
-  background-color: #d9d9d9;
-}
-.inputcol {
-  display: inline-block;
-}
-.inputbox {
-  margin-top: 5px;
-  margin-right: 20px;
-  width: 50px;
-}
-.dropdownlist {
-  margin-top: 5px;
-  width: 100px;
-}
-.addBox {
-  margin-left: 60px;
 }
 </style>
